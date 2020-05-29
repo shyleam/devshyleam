835c67cc1f566ccf0506c198a88f6a7cfa46c7c1
shyleam <shyleam.tech@gmail.com>
2020-05-29 16:41:13 +0700

    Updates

---
+++ precache-manifest.2b97979286a3e647b458095d3ac35dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6e8d65f9b6b9cf74d223449c5ae4b5",
+    "url": "/devshyleam/index.html"
+  },
+  {
+    "revision": "89a24233a1c66e250339",
+    "url": "/devshyleam/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aaf4c7c368839b1d5dcd",
+    "url": "/devshyleam/static/js/2.b7d58757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devshyleam/static/js/2.b7d58757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a24233a1c66e250339",
+    "url": "/devshyleam/static/js/main.e5b7be86.chunk.js"
+  },
+  {
+    "revision": "d4c71c493c4c3e450106",
+    "url": "/devshyleam/static/js/runtime-main.66a20962.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devshyleam/static/media/logo.5d5d9eef.svg"
+  }
+]);
